04e7d008a7097a54799757489da42bfc5bbe8292
Liz Burton <liz.burton147@gmail.com>
2017-10-28 16:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  var lineString = "The line is currently: ";
   if (katzDeliLine.length === 0) {
     return ("The line is currently empty.")
   } else {
   for (var i=0; i<katzDeliLine.length; i++)
-    return ("")
+    return (lineString += ( (i+1) + ". " + katzDeliLine[i]);
   }
 }
