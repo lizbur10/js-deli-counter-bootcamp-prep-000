1afc183f74029f230268886b8252d4c0a989819c
Liz Burton <liz.burton147@gmail.com>
2017-10-28 16:18:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(line) {
-
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return ("The line is currently empty.")
+  } else {
+  for (var i=0; i<katzDeliLine.length; i++)
+    return ("")
+  }
 }
