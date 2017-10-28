87e166a7b8b09710675171a66b4ffeca4c623c99
Liz Burton <liz.burton147@gmail.com>
2017-10-28 15:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
-  var position = katzDeliLine.length + 1;
+  var position = "Welcome, Ada. You are number " + katzDeliLine.length + 1 + " in line.";
   return position;
 }
